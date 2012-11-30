620b990e16fda04aa56639f47e2f8d7a7cd0df12
David Woodruff <mustang2009cobra@gmail.com>
2012-11-30 15:07:08 -0700

    Put tabs in the index.php document

--- first_mockup/js/renderer.js
+++ first_mockup/js/renderer.js
@@ -8,16 +8,14 @@ var Renderer = {
 		var source = Templates.dashboard;
 		var template = Handlebars.compile(source);
 
-		
-
-		$("#container").html(template(data));
+		$("#renderedContent").html(template(data));
 	},
 
 	renderGroupsPage : function(data){
 		var source = Templates.groups;
 		var template = Handlebars.compile(source);
 		
-		$("#container").html(template(data));		
+		$("#renderedContent").html(template(data));		
 	},
 
 	renderGroupPage : function(){
@@ -40,42 +38,42 @@ var Renderer = {
 			]
 		};
 
-		$("#container").html(template(data));
+		$("#renderedContent").html(template(data));
 	},
 
 	renderProfilePage : function() {
 		var source = Templates.profile;
 		var template = Handlebars.compile(source);
 	
-		$("#container").html(template());		
+		$("#renderedContent").html(template());		
 	},
 
 	renderStudySchedulePage : function() {
 		var source = Templates.studyschedule;
 		var template = Handlebars.compile(source);
 	
-		$("#container").html(template());	
+		$("#renderedContent").html(template());	
 	},
 
 	renderMeetingPage : function() {
 		var source = Templates.meetingdetails;
 		var template = Handlebars.compile(source);
 		
-		$("#container").html(template());	
+		$("#renderedContent").html(template());	
 	},
 	
 	renderSearchPage : function() { 
 		var source = Templates.search;
 		var template = Handlebars.compile(source);
 		
-		$("#container").html(template());		
+		$("#renderedContent").html(template());		
 	},
 
 	renderStudyTimePage : function() {
 		var source = Templates.studytime;
 		var template = Handlebars.compile(source);
 		
-		$("#container").html(template());	
+		$("#renderedContent").html(template());	
 	}
 
 	/*
