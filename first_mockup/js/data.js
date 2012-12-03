f1e721af725abf003cb6c24bcf8f811c54c17704
Chase Wasden <wazztone@gmail.com>
2012-12-03 16:34:06 -0700

    implemented group page navigation (sorry, Ogden, trying to get to meetings)

--- first_mockup/js/data.js
+++ first_mockup/js/data.js
@@ -14,40 +14,48 @@ var Data = {
 		//FAKE DATA FOR NOW UNTIL WE GET THE BACKEND INTERFACE GOING
 		var data = {
 			groups : [
-			 	{ name : "Math 112", numMembers : 3},
-			 	{ name : "Bio 221", numMembers : 5 },
-			 	{ name : "AmHtg 100", numMembers : 18 },
-			 	{ name : "AmHtg 100", numMembers : 18 },
-			 	{ name : "AmHtg 100", numMembers : 18 },
-			 	{ name : "AmHtg 100", numMembers : 18 },
-			 	{ name : "AmHtg 100", numMembers : 181 },
-			 	{ name : "AmHtg 100", numMembers : 1 },
-			 	{ name : "AmHtg 100", numMembers : 1 },
-			 	{ name : "AmHtg 100", numMembers : 2 },
-			 	{ name : "AmHtg 100", numMembers : 32 },
-			 	{ name : "AmHtg 100", numMembers : 234 },
-			 	{ name : "Other", numMembers : 35 },			
-			]	                                    	
+			 	{ id:"8ea7a781-0104-41ac-b82a-3f2cbe438c23", name : "Math 112", numMembers : 3},
+			 	{ id:"5d2e5f19-6328-46bd-b6c4-b366d99b20e1", name : "Bio 221", numMembers : 5 },
+			 	{ id:"f64169c3-5053-439b-810d-c72cf81929f1", name : "AmHtg 100", numMembers : 18 },
+			 	{ id:"5a18e523-47f5-419a-8534-2e37917bf5c7", name : "Other", numMembers : 35 }
+			]		
 		}
 		return data;
 	},
-	groupPage : function(){
+	groupPage : function(id){
 		//FAKE DATA FOR NOW UNTIL WE GET THE BACKEND INTERFACE GOING
 		var data = {
-			groupName: "Math 112",
-			groupSubject: "Math 112",
-			groupDescription : "The happenin place for all things Newtonian Calculus",
-			members : [
-				"You",
-				"John Cassidy",
-				"Johnny Depp"				
-			],
-			meetings: [
-				{ name: "HW #3", date: "Tomorrow 3pm" },
-				{ name: "HW #4, HW #5", date: "Thurs, Jan 5th"}		
-			]
+			"8ea7a781-0104-41ac-b82a-3f2cbe438c23":{
+				id:"8ea7a781-0104-41ac-b82a-3f2cbe438c23",
+				groupName: "Math 112",
+				groupSubject: "Math 112",
+				groupDescription : "The happenin place for all things Newtonian Calculus",
+				members : [
+					"You",
+					"John Cassidy",
+					"Johnny Depp"				
+				],
+				meetings: [
+					{ name: "HW #3", date: "Tomorrow 3pm" },
+					{ name: "HW #4, HW #5", date: "Thurs, Jan 5th"}		
+				]
+			},
+			"5d2e5f19-6328-46bd-b6c4-b366d99b20e1": {
+				id:"5d2e5f19-6328-46bd-b6c4-b366d99b20e1",
+				groupName: "Bio 221",
+				groupSubject: "Biology",
+				groupDescription : "A sweet bio class",
+				members : [
+					"You",
+					"Dude1"
+				],
+				meetings: [
+					{ name: "HW #14", date: "Wednesday 3pm" }
+				]
+			}
 		};
-		return data;
+		if(!data[id]) throw "this group does not exist";
+		return data[id];
 	},
 	
 	profilePage : function(person) {
@@ -69,5 +77,24 @@ var Data = {
 		else{
 			return otherPersonData;
 		}
-	}
+	},
+
+	// save group
+	// @return the group object with id, etc.
+	createGroup: function(name, description){
+
+	},
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
